a3a7ce8642269d3d424242e5b1f0b7abf1d4bd41
eduardgacea <110187683+eduardgacea@users.noreply.github.com>
2023-12-15 08:18:28 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,23 @@ import Cart from "./pages/Cart";
 import Wishlist from "./pages/Wishlist";
 
 function App() {
-	return (
-		<ProductsContextProvider>
-			<GlobalStyle />
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Layout />}>
-						<Route index element={<Navigate to="/home" replace />} />
-						<Route path="home" element={<Home />} />
-						<Route path="products" element={<Products />} />
-						<Route path="products/:productId" element={<Product />} />
-						<Route path="cart" element={<Cart />} />
-						<Route path="wishlist" element={<Wishlist />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
-		</ProductsContextProvider>
-	);
+  return (
+    <ProductsContextProvider>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Navigate to="/home" replace />} />
+            <Route path="home" element={<Home />} />
+            <Route path="products" element={<Products />} />
+            <Route path="products/:productId" element={<Product />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="wishlist" element={<Wishlist />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ProductsContextProvider>
+  );
 }
 
 export default App;
